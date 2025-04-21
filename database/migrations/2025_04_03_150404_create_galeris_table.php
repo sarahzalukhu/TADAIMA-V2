347cc8a35f3746f4183ae39b5d5122f627949245
bnsrsrg03 <binsarsiregar8a@gmail.com>
2025-04-21 20:11:28 +0700

    Filament
    Install

--- database/migrations/2025_04_03_150404_create_galeris_table.php
+++ database/migrations/2025_04_03_150404_create_galeris_table.php
@@ -4,16 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('galeris', function (Blueprint $table) {
             $table->id();
-            $table->string('image'); // Kolom untuk menyimpan gambar galeri
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('galeris');
